6594496810ad49c480efc5ea5202872be218b99a
Jariphiya <68010130@kmitl.ac.th>
2026-03-25 10:25:07 +0700

    p01 again cuz i forgot to save

---
+++ src/products/product.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from 'src/products/product.interface'
+
+@Injectable()
+export class productservice {
+    private readonly products = [
+        id
+    ]
+}
